0dc977f2357397544cd88d3ee31eaa078adbcce1
Marina Malich <e_aryukova@mail.ru>
2023-02-22 22:30:20 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 11879;
+        int bonus = 20;
+        int mile = ticketprice / bonus;
+
+        System.out.println("Начислено миллей " + mile);
+    }
+}
